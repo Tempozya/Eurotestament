23cc41750533c339e29ded427a797ba14bd10206
Артём Семёнов <timo.semenow@yandex.ru>
2021-11-15 23:05:33 +0300

    Revert ""
    This reverts commit d008622e37a96d011dc578aa6af87507c4c063a5.

--- Eurotestament/ClientForm/Transact.cs
+++ Eurotestament/ClientForm/Transact.cs
@@ -58,7 +58,7 @@ namespace Eurotestament
             var index = check_box.SelectedIndex;
             var culture = new CultureInfo("ru-RU");
             txtBalance.Text = Convert.ToDouble(checks.BalanceCheck[index]).ToString("C");
-       
+            
         }
 
     }
