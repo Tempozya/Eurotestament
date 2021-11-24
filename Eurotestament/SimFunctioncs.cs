552c9cb32253825a3e9ddc0ce603363fac9544e9
Артём Семёнов <timo.semenow@yandex.ru>
2021-11-24 21:58:56 +0300

    Возвращение

--- Eurotestament/SimFunctioncs.cs
+++ Eurotestament/SimFunctioncs.cs
@@ -42,7 +42,7 @@ namespace Eurotestament
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Login { get; set; }
-        public string email { get; set; }
+        public string Password { get; set; }
         public string PhoneNum { get; set; }
 
 
