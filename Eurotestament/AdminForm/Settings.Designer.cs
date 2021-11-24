32f538cbae7173f8897c9d43c2d8b2330644d949
Артём Семёнов <timo.semenow@yandex.ru>
2021-11-25 02:25:46 +0300

    Настрока симуляции

--- Eurotestament/AdminForm/Settings.Designer.cs
+++ Eurotestament/AdminForm/Settings.Designer.cs
@@ -29,20 +29,158 @@ namespace Eurotestament.AdminForm
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxControl1 = new Eurotestament.DesignControls.TextBoxControl();
+            this.textBoxControl2 = new Eurotestament.DesignControls.TextBoxControl();
+            this.textBoxControl3 = new Eurotestament.DesignControls.TextBoxControl();
+            this.textBoxControl4 = new Eurotestament.DesignControls.TextBoxControl();
+            this.comBoxControl1 = new Eurotestament.DesignControls.ComBoxControl();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(73, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Настройка симуляции";
+            // 
+            // textBoxControl1
+            // 
+            this.textBoxControl1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxControl1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBoxControl1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBoxControl1.BorderRadius = 0;
+            this.textBoxControl1.BorderSize = 2;
+            this.textBoxControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxControl1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxControl1.Location = new System.Drawing.Point(65, 54);
+            this.textBoxControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxControl1.Multiline = false;
+            this.textBoxControl1.Name = "textBoxControl1";
+            this.textBoxControl1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.textBoxControl1.PasswordChar = false;
+            this.textBoxControl1.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.textBoxControl1.PlaceholderText = "Время 1 дня(мс)";
+            this.textBoxControl1.Size = new System.Drawing.Size(228, 31);
+            this.textBoxControl1.TabIndex = 1;
+            this.textBoxControl1.Texts = "";
+            this.textBoxControl1.UnderlinedStyle = false;
+            // 
+            // textBoxControl2
+            // 
+            this.textBoxControl2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxControl2.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBoxControl2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBoxControl2.BorderRadius = 0;
+            this.textBoxControl2.BorderSize = 2;
+            this.textBoxControl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxControl2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxControl2.Location = new System.Drawing.Point(65, 93);
+            this.textBoxControl2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxControl2.Multiline = false;
+            this.textBoxControl2.Name = "textBoxControl2";
+            this.textBoxControl2.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.textBoxControl2.PasswordChar = false;
+            this.textBoxControl2.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.textBoxControl2.PlaceholderText = "Новых пользователей в день";
+            this.textBoxControl2.Size = new System.Drawing.Size(228, 31);
+            this.textBoxControl2.TabIndex = 2;
+            this.textBoxControl2.Texts = "";
+            this.textBoxControl2.UnderlinedStyle = false;
+            // 
+            // textBoxControl3
+            // 
+            this.textBoxControl3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxControl3.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBoxControl3.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBoxControl3.BorderRadius = 0;
+            this.textBoxControl3.BorderSize = 2;
+            this.textBoxControl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxControl3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxControl3.Location = new System.Drawing.Point(65, 132);
+            this.textBoxControl3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxControl3.Multiline = false;
+            this.textBoxControl3.Name = "textBoxControl3";
+            this.textBoxControl3.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.textBoxControl3.PasswordChar = false;
+            this.textBoxControl3.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.textBoxControl3.PlaceholderText = "Транзакций в день";
+            this.textBoxControl3.Size = new System.Drawing.Size(228, 31);
+            this.textBoxControl3.TabIndex = 3;
+            this.textBoxControl3.Texts = "";
+            this.textBoxControl3.UnderlinedStyle = false;
+            // 
+            // textBoxControl4
+            // 
+            this.textBoxControl4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxControl4.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBoxControl4.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBoxControl4.BorderRadius = 0;
+            this.textBoxControl4.BorderSize = 2;
+            this.textBoxControl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxControl4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxControl4.Location = new System.Drawing.Point(65, 171);
+            this.textBoxControl4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxControl4.Multiline = false;
+            this.textBoxControl4.Name = "textBoxControl4";
+            this.textBoxControl4.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.textBoxControl4.PasswordChar = false;
+            this.textBoxControl4.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.textBoxControl4.PlaceholderText = "Транзакций в день";
+            this.textBoxControl4.Size = new System.Drawing.Size(228, 31);
+            this.textBoxControl4.TabIndex = 4;
+            this.textBoxControl4.Texts = "";
+            this.textBoxControl4.UnderlinedStyle = false;
+            // 
+            // comBoxControl1
+            // 
+            this.comBoxControl1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.comBoxControl1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.comBoxControl1.BorderSize = 1;
+            this.comBoxControl1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.comBoxControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.comBoxControl1.ForeColor = System.Drawing.Color.DimGray;
+            this.comBoxControl1.IconColor = System.Drawing.Color.MediumSlateBlue;
+            this.comBoxControl1.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+            this.comBoxControl1.ListTextColor = System.Drawing.Color.DimGray;
+            this.comBoxControl1.Location = new System.Drawing.Point(65, 210);
+            this.comBoxControl1.MinimumSize = new System.Drawing.Size(200, 30);
+            this.comBoxControl1.Name = "comBoxControl1";
+            this.comBoxControl1.Padding = new System.Windows.Forms.Padding(1);
+            this.comBoxControl1.Size = new System.Drawing.Size(228, 30);
+            this.comBoxControl1.TabIndex = 5;
+            this.comBoxControl1.Texts = "День обновления данных";
             // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 653);
+            this.Controls.Add(this.comBoxControl1);
+            this.Controls.Add(this.textBoxControl4);
+            this.Controls.Add(this.textBoxControl3);
+            this.Controls.Add(this.textBoxControl2);
+            this.Controls.Add(this.textBoxControl1);
+            this.Controls.Add(this.label1);
             this.Name = "Settings";
             this.Text = "Настройки";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private DesignControls.TextBoxControl textBoxControl1;
+        private DesignControls.TextBoxControl textBoxControl2;
+        private DesignControls.TextBoxControl textBoxControl3;
+        private DesignControls.TextBoxControl textBoxControl4;
+        private DesignControls.ComBoxControl comBoxControl1;
     }
 }
